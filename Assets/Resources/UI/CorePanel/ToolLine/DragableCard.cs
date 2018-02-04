2e9bb783312e34028606ca9ae56ba0f4ac37b875
Yoshiki321 <43903058@qq.com>
2018-02-05 00:19:55 +0800

    上传

---
+++ Assets/Resources/UI/CorePanel/ToolLine/DragableCard.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DragableCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+	public float xDis;
+	public float yDis;
+
+    // begin dragging
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+		SetDraggedPosition(eventData);
+    }
+
+    // during dragging
+    public void OnDrag(PointerEventData eventData)
+    {
+		SetDraggedPositionIn (eventData);
+    }
+
+    // end dragging
+    public void OnEndDrag(PointerEventData eventData)
+    {
+		SetDraggedPosition(eventData);
+    }
+
+    /// <summary>
+    /// set position of the dragged game object
+    /// </summary>
+    /// <param name="eventData"></param>
+
+    private void SetDraggedPosition(PointerEventData eventData)
+    {
+        var rt = gameObject.GetComponent<RectTransform>();
+        Vector3 globalMousePos;
+		xDis = rt.anchoredPosition3D.x - eventData.position.x;
+		yDis = rt.anchoredPosition3D.y - eventData.position.y;
+    }
+	private void SetDraggedPositionIn(PointerEventData eventData)
+	{
+		gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(eventData.position.x + xDis,eventData.position.y + yDis);
+	}
+}
---
+++ Assets/Resources/UI/Currency/SelectColor/ColorManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class ColorManager : MonoBehaviour
+{
+    RectTransform rt;
+
+    private ColorRGB CRGB;
+    private ColorPanel CP;
+    private ColorCircle CC;
+
+    public Slider sliderCRGB;
+    public Image colorShow;
+
+
+    void OnDisable()
+    {
+        if (CC != null) CC.getPos -= CC_getPos;
+    }
+
+    private void CC_getPos(Vector2 pos)
+    {
+        Color getColor = CP.GetColorByPosition(pos);
+        colorShow.color = getColor;
+
+        ColorVO vo = new ColorVO();
+        vo.color = getColor;
+        vo.slider = _sliderValue;
+        vo.position = CP.colorPosition;
+
+        acc?.Invoke(vo);
+    }
+    
+    public void SetColor(ColorVO vo)
+    {
+        CC.rt.anchoredPosition = vo.position;
+        sliderCRGB.value = vo.slider;
+
+        OnCRGBValueChanged(vo.slider);
+    }
+
+    Action<ColorVO> acc;
+
+    public void GetColor(Action<ColorVO> tt)
+    {
+        acc = tt;
+    }
+
+    private void Awake()
+    {
+        rt = GetComponent<RectTransform>();
+
+        CRGB = GetComponentInChildren<ColorRGB>();
+        CP = GetComponentInChildren<ColorPanel>();
+        CC = GetComponentInChildren<ColorCircle>();
+
+        sliderCRGB.onValueChanged.AddListener(OnCRGBValueChanged);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        CC.getPos += CC_getPos;
+    }
+
+    private void OnEnable()
+    {
+        if (CC != null) CC.getPos += CC_getPos;
+    }
+
+    private float _sliderValue = 0;
+
+    void OnCRGBValueChanged(float value)
+    {
+        _sliderValue = value;
+
+        Color endColor = CRGB.GetColorBySliderValue(value);
+        CP.SetColorPanel(endColor);
+        CC.setShowColor();
+    }
+}
